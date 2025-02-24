96bdd07554d771c8047d63362d8f7ee3c63fa335
Adi Makkar <adimakkar.ca@gmail.com>
2025-02-24 11:31:57 -0500

    first commit

---
+++ src/extensions/probAccess/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IProbAccessApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'ProbAccessApplicationCustomizerStrings' {
+  const strings: IProbAccessApplicationCustomizerStrings;
+  export = strings;
+}
